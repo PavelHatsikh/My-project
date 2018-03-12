534a2ed3a7b3b26be6f6f11c8dfe23a9a4e2c1e1
PavelHatsikh <bos07@inbox.ru>
2018-03-12 19:01:49 +0300

    Project with class

--- Class_realization_5_variant/Receipt.cs
+++ Class_realization_5_variant/Receipt.cs
@@ -8,6 +8,52 @@ namespace Class_realization_5_variant
 {
     class Receipt
     {
+        private string product_naming;
+        private string cipher;
+        private int count_of_receipts;
 
+
+        public string Product_naming { get; set; }
+        public string Cipher { get; set; }
+        public int Count_of_receipts { get; set; }
+        public Receipt()
+        {
+            Product_naming = null;
+            Cipher = null;
+            Count_of_receipts = 0;
+        }
+
+        public Receipt(string product_naming, string cipher, int count_of_receipts)
+        {
+            Product_naming = product_naming;
+            Cipher = cipher;
+            Count_of_receipts = count_of_receipts;
+        }
+
+        public virtual void Input()
+        {
+            Console.WriteLine("(- - - - - - - - - - Input description of class- - - -  - - - - -)");
+            Console.WriteLine();
+            Console.WriteLine("Название продукта: ");
+            Product_naming = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Шифр продукта: ");
+            Cipher = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Количество едениц изделия: ");
+            Count_of_receipts = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine("(- - - - - - - - - - - - - Output class - - - - - - - - - - -)");
+            Console.WriteLine("Название продукта: " + Product_naming);
+            Console.WriteLine();
+            Console.WriteLine("Шифр продукта: " + Cipher);
+            Console.WriteLine();
+            Console.WriteLine("Количество едениц изделия: " + Count_of_receipts);
+            Console.WriteLine();
+        }
     }
 }
